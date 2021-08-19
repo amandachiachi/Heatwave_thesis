34fb704b34475ea02c5636ec430299f0ce85ac55
Amanda Chiach <amandachiachi@gmail.com>
2021-08-19 06:36:48 -0700

    Used patchwork to combine plots and changed colors

--- scripts/Abalone_analysis_script.R
+++ scripts/Abalone_analysis_script.R
@@ -5,8 +5,6 @@
 
 # Load Libraries 
 
-install.packages("ggsci")
-
 library('here')
 library('stringr')
 library('performance')
@@ -16,6 +14,7 @@ library('survival')
 library('survminer')
 library('ggsci')
 library('lubridate')
+library('patchwork')
 
 # Bring in data 
 # rates, mortality, CI, diet trial
@@ -121,11 +120,11 @@ weight_plot <- summarypchange%>%
   ggplot(aes(x = size_class, y = weight_avgpchange, color = treatment))+
   geom_point()+
   geom_errorbar(aes(ymin = weight_avgpchange - weight_sepchange, ymax = weight_avgpchange + weight_sepchange), width = 0.2)+
-  labs(x = "Size Class",
+  labs(x = "",
        y = "Average change in weight (g)")+ # re-label y label 
   theme_light()+ # remove the background color and make the plot look a bit simpler
   theme(axis.title = element_text(size = 15))+
-  scale_color_manual(values = c("#AFEEEE", "#990000"))
+  scale_color_manual(values = c("#a8a8a8", "#990000"))
 
 weight_plot
 
@@ -138,8 +137,12 @@ length_plot <- summarypchange%>%
        y = "Average change in length (mm)")+ # re-label y label 
   theme_light()+ # remove the background color and make the plot look a bit simpler
   theme(axis.title = element_text(size = 15))+
-  scale_color_manual(values = c("#AFEEEE", "#990000"))
+  scale_color_manual(values = c("#a8a8a8", "#990000"))
 length_plot
+# combine plots 
+weight_plot/length_plot + plot_layout( guides = 'collect')
+ggsave(filename = "output/change_weight_length.png", width = 12, height = 10, dpi = 300)
+
 # check to see if this is correct - after this, if clean we can write it
 
 #### survivorship curve ####
@@ -151,57 +154,69 @@ length_plot
 # treatment mortality with size class 30 
 mort_30<- mortality%>%
   filter(size_class == "30")
-view(mort_30)
+#view(mort_30)
 fit1 <- survfit(Surv(time, status) ~ treatment, data = mort_30)
-ggsurv(fit1)
-ggsurvplot(fit1, data = mortality,
+#ggsurv(fit1)
+p1<- ggsurvplot(fit1, data = mortality,
            conf.int = TRUE,
            pval = FALSE, 
            fun = "pct", 
            risk.table = FALSE, 
            size = 1, 
            linetype = "strata", 
-           palette = c("#AFEEEE", 
-                       "#990000"),
+           palette = c("#a8a8a8", 
+                       "#661100"),
            legend = "bottom", 
-           legend.title = "Survivorship Size Class 30mm", 
+           legend.title = "",
+           xlab = "",
+           ylab = "",
            legend.labs = c("ambient", "heatwave"))
+p1plot <- p1$plot + labs(title = "A                                   30mm")
 
 # treatment mortality with size class 60 
 mort_60<- mortality%>%
   filter(size_class == "60")
 fit1 <- survfit(Surv(time, status) ~ treatment, data = mort_60)
-ggsurv(fit1)
-ggsurvplot(fit1, data = mortality,
+#ggsurv(fit1)
+p2<-ggsurvplot(fit1, data = mortality,
            conf.int = TRUE,
            pval = FALSE, 
            fun = "pct", 
            risk.table = FALSE, 
            size = 1, 
            linetype = "strata", 
-           palette = c("#AFEEEE", 
-                       "#990000"),
+           palette = c("#a8a8a8", 
+                       "#661100"),
            legend = "bottom", 
-           legend.title = "Survivorship Size Class 60mm", 
+           legend.title = "", 
+           xlab = "", 
            legend.labs = c("ambient", "heatwave"))
+p2plot <- p2$plot + labs(title = "B                                   60mm")
 
 # treatment mortality with size class 90
 mort_90<- mortality%>%
   filter(size_class == "90")
 fit1 <- survfit(Surv(time, status) ~ treatment, data = mort_90)
-ggsurv(fit1)
-ggsurvplot(fit1, data = mortality,
+#ggsurv(fit1)
+p3<-ggsurvplot(fit1, data = mortality,
            conf.int = TRUE,
            pval = FALSE, 
            fun = "pct", 
            risk.table = FALSE, 
            size = 1, 
            linetype = "strata", 
-           palette = c("#AFEEEE", 
-                       "#990000"),
+           palette = c("#a8a8a8", 
+                       "#661100"),
            legend = "bottom", 
-           legend.title = "Survivorship Size Class 90mm", 
+           legend.title = "", 
+           ylab = "",
            legend.labs = c("ambient", "heatwave"))
+p3plot <- p3$plot + labs(title = "C                                   90mm")
+
+# use patchwork to combine all mortality plots and make them pub quality 
+(p1plot/p2plot/p3plot)+ plot_layout( guides = 'collect')
+ggsave(filename = "output/survplot_all.png", width = 9, height = 10, dpi = 300)
+
 
 # treatment mortality with all sizes
 fit1 <- survfit(Surv(time, status) ~ treatment, data = mortality)
@@ -269,6 +284,8 @@ dietrates<-dietrates%>%
 pH_data <- read_csv(here("data", "Cultured_pH.csv"))
 
 pH_data$datetimes <- mdy_hm(pH_data$datetimes)
+
+# filter(pH_data, between(datetimes, as.Date("2020-01-01 12:00:00"), as.Date("2020-05-01 12:00:00"))) 
 
 pH_plot <- pH_data%>%
   ggplot(aes(x = datetimes, y = pH))+
